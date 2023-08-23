6af985286c3ee9715dc5edca7e6e22b0ea69d2fc
megalab <mega1ab@ya.ru>
2023-08-24 02:39:26 +0300

    css customizing: fonts, labels, tabs

--- main.py
+++ main.py
@@ -8,10 +8,19 @@ app = QApplication(sys.argv)
 # colors app: #272829, #61677A, #D8D9DA, #FFF6E0
 
 app.setStyleSheet(
-    # "QMainWindow  { background-color: black; }"
+    # "QMainWindow  { background-color: #F1F0E8; }
+    "QTabWidget { font-family: sans-serif; font-size: 10pt; }"
     "QTabWidget::pane { border-top: 2px solid #272829; }"
     "QTabBar::tab:selected { background: #272829; color: #FFF6E0; }"
-    " QTabBar::tab:hover { background: #61677A; color: #FFF6E0C; }"
+    "QTabBar::tab:hover { background: #61677A; color: #FFF6E0C; }"
+    "QPushButton { padding: 6px; color: #27282; font-family: sans-serif; font-size: 10pt; }"
+    "QLineEdit { margin: 0; font-size: 12pt; }"
+    "QLabel { font-family: sans-serif; font-size: 10pt; }"
+    "QComboBox { font-family: sans-serif; font-size: 10pt; }"
+    "QSpinBox { font-family: sans-serif; font-size: 10pt; }"
+    "QDoubleSpinBox { font-family: sans-serif; font-size: 10pt; }"
+    "QGroupBox { font-size: 9pt; }"
+    "QGroupBox::title { subcontrol-origin: margin; subcontrol-position: top center; padding: 0 3px; color: #61677A; }"
 )
 
 # with open('styles.qss', 'r') as f:
