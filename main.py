950ee6d34840264bfbf44c1ebd2f370404a1e497
megalab <mega1ab@ya.ru>
2023-08-24 01:13:45 +0300

    shorten the interface module and moved the declared widgets to the corresponding file

--- main.py
+++ main.py
@@ -5,11 +5,14 @@ from PyQt5.QtWidgets import QApplication
 
 app = QApplication(sys.argv)
 
-# app.setStyleSheet(
-#     "QMainWindow  { background-color: black; }"
-#     "QTabWidget::pane { border-top: 2px solid #0d192b; }"
-#     "QTabBar::tab:selected, QTabBar::tab:hover { background: #0aa09e; color: black; }"
-# )
+# colors app: #272829, #61677A, #D8D9DA, #FFF6E0
+
+app.setStyleSheet(
+    # "QMainWindow  { background-color: black; }"
+    "QTabWidget::pane { border-top: 2px solid #272829; }"
+    "QTabBar::tab:selected { background: #272829; color: #FFF6E0; }"
+    " QTabBar::tab:hover { background: #61677A; color: #FFF6E0C; }"
+)
 
 # with open('styles.qss', 'r') as f:
 #     style = f.read()
